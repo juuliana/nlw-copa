9f767d61e16acaeeed5aee8536a0738191fc6ccd
juuliana <jumaju10@hotmail.com>
2022-11-02 16:08:20 -0300

    feat: create new request of post and gets

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
+import { z } from "zod";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -15,9 +17,39 @@ async function bootstrap() {
     origin: true,
   });
 
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
     return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generateCode = new ShortUniqueId({ length: 6 });
+    const code = String(generateCode()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333 /*host: "0.0.0.0"*/ });
