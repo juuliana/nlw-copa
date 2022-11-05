4913915e44095dec2d80a68902db90d0891d6dca
juuliana <jumaju10@hotmail.com>
2022-11-05 16:49:11 -0300

    feat: create get polls of user

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -107,4 +107,48 @@ export async function pollRoutes(fastify: FastifyInstance) {
       return reply.status(201).send();
     }
   );
+
+  fastify.get(
+    "/polls",
+    {
+      onRequest: [authenticate],
+    },
+    async (request) => {
+      const polls = await prisma.poll.findMany({
+        where: {
+          participants: {
+            some: {
+              userId: request.user.sub,
+            },
+          },
+        },
+        include: {
+          _count: {
+            select: {
+              participants: true,
+            },
+          },
+          participants: {
+            select: {
+              id: true,
+              user: {
+                select: {
+                  avatarUrl: true,
+                },
+              },
+            },
+            take: 4,
+          },
+          owner: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+      return { polls };
+    }
+  );
 }
