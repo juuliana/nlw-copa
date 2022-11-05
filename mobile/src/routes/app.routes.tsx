cd17fc2cf6dc03df09eb86c992d4931a1498fcf0
juuliana <jumaju10@hotmail.com>
2022-11-05 18:33:17 -0300

    feat: create app routes

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,58 @@
+import { useTheme } from "native-base";
+import { PlusCircle, SoccerBall } from "phosphor-react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Find, New, Polls } from "../screens";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  const { colors, sizes } = useTheme();
+
+  const size = sizes[6];
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: "beside-icon",
+        tabBarActiveTintColor: colors.yellow[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+          display: "flex",
+          position: "absolute",
+          height: sizes[22],
+          borderTopWidth: 0,
+          backgroundColor: colors.gray[800],
+        },
+        tabBarItemStyle: {
+          position: "relative",
+        },
+      }}
+    >
+      <Screen
+        name="new"
+        component={New}
+        options={{
+          tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} />,
+          tabBarLabel: "Novo bolão",
+        }}
+      />
+      <Screen
+        name="polls"
+        component={Polls}
+        options={{
+          tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
+          tabBarLabel: "Meus bolões",
+        }}
+      />
+      <Screen
+        name="find"
+        component={Find}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+    </Navigator>
+  );
+}
