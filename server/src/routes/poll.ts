21f8b01bbc024f86d08412032532620cfcf34adf
juuliana <jumaju10@hotmail.com>
2022-11-05 16:29:39 -0300

    feat: create join at poll

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -3,6 +3,7 @@ import ShortUniqueId from "short-unique-id";
 import { FastifyInstance } from "fastify";
 
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function pollRoutes(fastify: FastifyInstance) {
   fastify.get("/polls/count", async () => {
@@ -20,13 +21,90 @@ export async function pollRoutes(fastify: FastifyInstance) {
     const generateCode = new ShortUniqueId({ length: 6 });
     const code = String(generateCode()).toUpperCase();
 
-    await prisma.poll.create({
-      data: {
-        title,
-        code,
-      },
-    });
+    try {
+      await request.jwtVerify();
+
+      await prisma.poll.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          participants: {
+            create: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      await prisma.poll.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    }
 
     return reply.status(201).send({ code });
   });
+
+  fastify.post(
+    "/polls/:id/join",
+    {
+      onRequest: [authenticate],
+    },
+    async (request, reply) => {
+      const joinPollBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPollBody.parse(request.body);
+
+      const poll = await prisma.poll.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!poll) {
+        return reply.status(400).send({
+          message: "Bolão não encontrado.",
+        });
+      }
+
+      if (poll.participants.length > 0) {
+        return reply.status(400).send({
+          message: "Você já está nesse bolão.",
+        });
+      }
+
+      if (!poll.ownerId) {
+        await prisma.poll.update({
+          where: {
+            id: poll.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        });
+      }
+
+      await prisma.participant.create({
+        data: {
+          pollId: poll.id,
+          userId: request.user.sub,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
 }
