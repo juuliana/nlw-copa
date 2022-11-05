d1ca29b5d4e180089b95e894077aafecabe0c436
juuliana <jumaju10@hotmail.com>
2022-11-05 17:17:25 -0300

    feat: create guess

--- server/src/routes/guess.ts
+++ server/src/routes/guess.ts
@@ -1,10 +1,94 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get("/guesses/count", async () => {
     const count = await prisma.guess.count();
     return { count };
   });
+
+  fastify.post(
+    "/polls/:pollId/games/:gameId/guesses",
+    {
+      onRequest: [authenticate],
+    },
+    async (request, reply) => {
+      const createGuessParams = z.object({
+        pollId: z.string(),
+        gameId: z.string(),
+      });
+
+      const createGuessBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+      });
+
+      const { pollId, gameId } = createGuessParams.parse(request.params);
+      const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(
+        request.body
+      );
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_pollId: {
+            pollId,
+            userId: request.user.sub,
+          },
+        },
+      });
+
+      if (!participant) {
+        return reply.status(400).send({
+          message: "Você não faz parte desse bolão.",
+        });
+      }
+
+      const guess = await prisma.guess.findUnique({
+        where: {
+          participantId_gameId: {
+            participantId: participant.id,
+            gameId,
+          },
+        },
+      });
+
+      if (guess) {
+        return reply.status(400).send({
+          message: "Você já enviou o seu palpite.",
+        });
+      }
+
+      const game = await prisma.game.findUnique({
+        where: {
+          id: gameId,
+        },
+      });
+
+      if (!game) {
+        return reply.status(400).send({
+          message: "Jogo não encontrado.",
+        });
+      }
+
+      if (game.date < new Date()) {
+        return reply.status(400).send({
+          message: "Você não pode enviar um palpite depois da data do jogo.",
+        });
+      }
+
+      await prisma.guess.create({
+        data: {
+          gameId,
+          participantId: participant.id,
+          firstTeamPoints,
+          secondTeamPoints,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
 }
