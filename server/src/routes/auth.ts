01ceed51650286b7ea7e3685251249f490cea77e
juuliana <jumaju10@hotmail.com>
2022-11-05 14:28:59 -0300

    feat: organize routes

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,3 @@
+import { FastifyInstance } from "fastify";
+
+export async function authRoutes(fastify: FastifyInstance) {}
---
+++ server/src/routes/poll.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
+import { FastifyInstance } from "fastify";
+
+import { prisma } from "../lib/prisma";
+
+export async function pollRoutes(fastify: FastifyInstance) {
+  fastify.get("/polls/count", async () => {
+    const count = await prisma.poll.count();
+    return { count };
+  });
+
+  fastify.post("/polls", async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPollBody.parse(request.body);
+
+    const generateCode = new ShortUniqueId({ length: 6 });
+    const code = String(generateCode()).toUpperCase();
+
+    await prisma.poll.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
+  });
+}
