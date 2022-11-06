f9eee68ba4b512578f4b0cc831d1e0ca570c6654
juuliana <jumaju10@hotmail.com>
2022-11-06 11:30:18 -0300

    feat: add poll card

--- mobile/src/screens/polls.tsx
+++ mobile/src/screens/polls.tsx
@@ -1,11 +1,50 @@
-import { VStack, Icon } from "native-base";
+import { useCallback, useState } from "react";
 import { Octicons } from "@expo/vector-icons";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { Button, Header } from "../components";
+import { api } from "../services/api";
+import {
+  Button,
+  Header,
+  PollCard,
+  Loading,
+  PollCardProps,
+  EmptyPollList,
+} from "../components";
 
 export function Polls() {
+  const [polls, setPolls] = useState<PollCardProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPolls();
+    }, [])
+  );
+
+  async function fetchPolls() {
+    try {
+      setIsLoading(true);
+      const { data } = await api.get("/polls");
+
+      setPolls(data.polls);
+    } catch (error) {
+      toast.show({
+        title: "Não foi possível carregar os bolões.",
+        placement: "top",
+        bgColor: "red.500",
+      });
+
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -27,6 +66,17 @@ export function Polls() {
           onPress={() => navigate("find")}
         />
       </VStack>
+
+      {isLoading && <Loading />}
+
+      <FlatList
+        data={polls}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PollCard data={item} />}
+        ListEmptyComponent={() => <EmptyPollList />}
+        _contentContainerStyle={{ pb: 10 }}
+        px={5}
+      />
     </VStack>
   );
 }
